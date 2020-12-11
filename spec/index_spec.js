b1e38232ff4a16cd504a8772f3376f11d188ee2f
Siyabulela <siyabulelakhumalo079@gmail.com>
2020-12-11 14:04:45 +0200

    Used prettier formatter

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,10 +1,10 @@
-const {buddy} = require("../src/index")
+const { buddy } = require("../src/index");
 
-describe("Given two musical tones, user should guess how many semetones between those two notes", ()=> {
-    it("should randomly print an array of two notes", ()=>{
-            expect(buddy.selectNotes()).toBeDefined();
-        })
-    it("should calculate semitones and return a value", ()=>{
-        expect(buddy.checkAnswer(1)).toMatch(/true|false/);
-    })
-})
+describe("Given two musical tones, user should guess how many semetones between those two notes", () => {
+  it("should randomly print an array of two notes", () => {
+    expect(buddy.selectNotes()).toBeDefined();
+  });
+  it("should compare user input to semitones returned and return true if they match, or false if they don't match", () => {
+    expect(buddy.checkAnswer(1)).toMatch(/true|false/);
+  });
+});
