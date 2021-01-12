6d603007583aa63fb23e5bfac3e561399dcaaad2
Siyabulela <siyabulelakhumalo079@gmail.com>
2021-01-12 10:04:03 +0200

    Semitone Difference - Advanced Algorithm

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,31 +1,14 @@
-const { buddy } = require("../src/js/script");
-const { JSDOM } = require("jsdom");
+const { JamBuddy } = require("../src/index");
+let buddy = new JamBuddy();
 
 describe("Given two musical tones, user should guess how many semetones between those two notes", () => {
   it("should randomly print an array of two notes", () => {
     expect(buddy.selectNotes()).toBeDefined();
   });
-});
-describe(`index.html`, function () {
-  let browser;
-
-  beforeEach(function (done) {
-    JSDOM.fromFile(`./src/index.html`).then(function (res) {
-      browser = res;
-      done();
-    });
+  it("should compare user input to semitones returned and return true if they match, or false if they don't match", () => {
+    expect(buddy.checkAnswer(1)).toMatch(/true|false/);
   });
-
-  afterEach(function () {
-    browser.window.close();
-  });
-
-  it(`should have </div> element`, function () {
-    let div = browser.window.document.querySelector(`div`);
-    expect(div).not.toBe(null);
-  });
-  it(`should have </script> element`, function () {
-    let script = browser.window.document.querySelector(`script`);
-    expect(script).not.toBe(null);
+  it("should check if checkAnswer function exist", function () {
+    expect(buddy.checkAnswer).toBeDefined();
   });
 });
