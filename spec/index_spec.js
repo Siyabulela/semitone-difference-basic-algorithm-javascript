2d04771bd8c5f98d4352e73d66fd73582efbb988
Siyabulela <siyabulelakhumalo079@gmail.com>
2021-01-23 01:26:59 +0200

    Gui that is more awesome

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,4 +1,4 @@
-const { buddy } = require("../src/js/script");
+const { buddy, myFunction, showNotes, reveal } = require("../src/js/script");
 const { JSDOM } = require("jsdom");
 const fs = require("fs");
 const html = fs.readFileSync("src/index.html", "utf-8");
@@ -14,6 +14,15 @@ describe("Given two musical tones, user should guess how many semetones between 
   });
   it("should check if shuffle function exist", () => {
     expect(buddy.selectNotes).toBeDefined();
+  });
+  it("should check if myFunction function exist", () => {
+    expect(myFunction).toBeDefined();
+  });
+  it("should check if showNotes function exist", () => {
+    expect(showNotes).toBeDefined();
+  });
+  it("should check if reveal function exist", () => {
+    expect(reveal).toBeDefined();
   });
   it("should compare user input to semitones returned and return true if they match, or false if they don't match", () => {
     expect(buddy.checkAnswer(1)).toMatch(/true|false/);
--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,9 @@ class JamBuddy {
       `Gb`,
       `Ab`,
     ]),
-      (this.selectedSemi = []);
+      (this.selectedSemi = []),
+      (this.answer = 0),
+      (this.streak = 0);
   }
   shuffle() {
     let newNotes = [...this.noteCircle];
@@ -55,10 +57,15 @@ class JamBuddy {
       this.noteCircle.indexOf(this.selectedSemi[0]) -
         this.noteCircle.indexOf(this.selectedSemi[1])
     );
+    this.answer = returnedNum;
     if (userInput == returnedNum) {
       return true;
     }
     return false;
+  }
+  difference() {
+    this.checkAnswer();
+    return this.answer;
   }
 }
 let buddy = new JamBuddy();
@@ -66,14 +73,67 @@ let buddy = new JamBuddy();
 function myFunction() {
   let x = document.getElementById("num").value;
   let correct = buddy.checkAnswer(x);
+  document.getElementById(`streakDefault`).style.visibility = `hidden`;
 
   if (correct == true) {
+    buddy.streak++;
+    document.getElementById("explanation").innerHTML = reveal();
     alert("You got it right. Well Done!");
-    location.reload();
-  } else alert("Wrong answer! Try again");
+  } else {
+    alert("Wrong answer! Try again");
+    buddy.streak = 0;
+  }
+  document.getElementById("streak").innerHTML = `Steak: ` + buddy.streak;
 }
-
 function showNotes() {
   document.getElementById("demo").innerHTML = buddy.selectNotes();
+  document.getElementById("diff").innerHTML = ``;
+  document.getElementById("explanation").innerHTML = ``;
 }
-module.exports = { buddy };
+function reveal() {
+  let notes = [
+    `A`,
+    `A#/Bb`,
+    `B`,
+    `C`,
+    `C#/Db`,
+    `D`,
+    `D#/Eb`,
+    `E`,
+    `F`,
+    `F#/Gb`,
+    `G`,
+    `G#/Ab`,
+  ];
+  let x = document.getElementById("demo").innerHTML.split(`,`);
+  let str = "";
+
+  for (let i = 0; i < 2; i++) {
+    if (x[i] == "Ab" || x[i] == "G#") {
+      x[i] = "G#/Ab";
+    } else if (x[i] == "Bb" || x[i] == "A#") {
+      x[i] = "A#/Bb";
+    } else if (x[i] == "Db" || x[i] == "C#") {
+      x[i] = "C#/Db";
+    } else if (x[i] == "Eb" || x[i] == "D#") {
+      x[i] = "D#/Eb";
+    } else if (x[i] == "Gb" || x[i] == "F#") {
+      x[i] = "F#/Gb";
+    }
+  }
+
+  for (let i = 0; i < notes.length; i++) {
+    if (x[0] === notes[i]) {
+      str = str + notes[i].fontcolor(`red`) + ` | `;
+    } else if (x[1] === notes[i]) {
+      str = str + notes[i].fontcolor(`red`) + ` | `;
+    } else {
+      str = str + notes[i] + ` | `;
+    }
+  }
+  document.getElementById("explanation").innerHTML = str;
+  document.getElementById("diff").innerHTML =
+    `Final Answer: ` + buddy.difference();
+  return str;
+}
+module.exports = { buddy, myFunction, showNotes, reveal };
